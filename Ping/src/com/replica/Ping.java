fd2bd08c9ea73da2e415a32fa856dbf9b52c0fc6
Thai Nguyen <thainguyen1809@gmail.com>
2011-04-04 00:09:00 +0700

    Ping

--- Ping/src/com/replica/Ping.java
+++ Ping/src/com/replica/Ping.java
@@ -1,7 +1,11 @@
 package com.replica;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.util.Enumeration;
 
@@ -9,20 +13,51 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Ping extends Activity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Thread sThread = new Thread(new TCPServer());
-        sThread.start();
+        final int SERVERPORT = 4444;
+        final Handler mHandler = new Handler();
+        
+        new Thread(new Runnable() {
+            public void run() {
+            	try {
+                    ServerSocket serverSocket = new ServerSocket(SERVERPORT);
+                    while (true) {
+                    	Socket client = serverSocket.accept();
+                    	try {
+                    		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                            final String str = in.readLine();
+                            final String clientIpAddress = client.getInetAddress().getHostAddress();
+                            Log.d("TCP", "S: Received: '" + str + "'");
+                            final Runnable mUpdateResults = new Runnable() {
+                                public void run() {
+                                	Toast.makeText(Ping.this, "Message from IP" + clientIpAddress + ": " + str, Toast.LENGTH_SHORT).show();
+                                }
+                            };
+                            mHandler.post(mUpdateResults);
+                    	} catch(Exception e) {}
+                  	  	finally {
+                  	  		client.close();
+                  	  	}
+                    }
+            	} catch (Exception e) {}
+            }
+        }).start();
+
         try {
                Thread.sleep(100);
           } catch (InterruptedException e) {}
@@ -39,6 +74,7 @@ public class Ping extends Activity {
 				}
 			}
 		});
+		
     }
 
 	public String getLocalIpAddress() throws SocketException {
